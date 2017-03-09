71a27a0880815756297f0db206f0faf5418497a9
dottoresole <dr.sunny.exet@gmail.com>
2017-03-09 14:17:51 +0200

    Удалил неиспользуемый код

--- views/personal/payer-statistic.php
+++ views/personal/payer-statistic.php
@@ -5,108 +5,11 @@ use yii\helpers\Url;
 use PhpOffice\PhpWord\PhpWord;
 use PhpOffice\PhpWord\IOFactory;
 
-
 $this->title = 'Информация';
-   $this->params['breadcrumbs'][] = 'Информация';
+$this->params['breadcrumbs'][] = $this->title;
+
 /* @var $this yii\web\View */
 ?>
-
-<?php /* if ($InformsProvider->getTotalCount() > 0) { ?>
-    <div class="modal fade">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Оповещения</h4>
-          </div>
-          <div class="modal-body">
-            <?= GridView::widget([
-                'dataProvider' => $InformsProvider,
-                'summary' => false,
-                'showHeader' => false,
-                'columns' => [
-                    // 'id',
-                    // 'contract_id',
-                    // 'from',
-                    'date',
-                    'text:ntext',
-                    'program_id',
-                    // 'read',
-
-                    ['class' => 'yii\grid\ActionColumn',
-                        'template' => '{permit} {view}',
-                         'buttons' =>
-                             [
-                                 'permit' => function ($url, $model) {
-                                     return Html::a('<span class="glyphicon glyphicon-ok"></span>', Url::to(['/informs/read', 'id' => $model->id]), [
-                                         'title' => Yii::t('yii', 'Отметить как прочитанное'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-                             ]
-                     ],
-                ],
-            ]); ?>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div>
-<?php } ?>
-
-
-<?php if ($CooperateProvider->getTotalCount() > 0) { ?>
-    <div class="modal fade">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Новые организации</h4>
-          </div>
-          <div class="modal-body">
-           <p>Эти организации желают с вами сотрудничать</p>
-            <?= GridView::widget([
-                'dataProvider' => $CooperateProvider,
-                'summary' => false,
-                'showHeader' => false,
-                'columns' => [
-                     'organization_id',
-
-                    ['class' => 'yii\grid\ActionColumn',
-                        'controller' => 'cooperate',
-                        'template' => '{view} {read}',
-                         'buttons' =>
-                             [
-                                 'permit' => function ($url, $model) {
-                                     return Html::a('<span class="glyphicon glyphicon-ok"></span>', Url::to(['/payers/cooperateok', 'id' => $model->id]), [
-                                         'title' => Yii::t('yii', 'Одобрить'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-
-                                'terminate' => function ($url, $model) {
-                                    return Html::a('<span class="glyphicon glyphicon-remove"></span>', Url::to(['/payers/cooperateno', 'id' => $model->id]), [
-                                         'title' => Yii::t('yii', 'Отказать'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-
-                                'read' => function ($url, $model) {
-                                     return Html::a('<span class="glyphicon glyphicon-check"></span>', Url::to(['/cooperate/read', 'id' => $model->id]), [
-                                         'title' => Yii::t('yii', 'Отметить как прочитанное'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-                             ]
-                     ],
-                ],
-            ]); ?>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div>
-<?php } */ ?>
-
-
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-4 col-md-offset-1 well">
