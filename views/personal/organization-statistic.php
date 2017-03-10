940b753a24c8bf7e665f7e2bec28cdea62d6be88
dottoresole <dr.sunny.exet@gmail.com>
2017-03-10 10:28:26 +0200

    Удаление неиспользуемого кода (организация статистика)

--- views/personal/organization-statistic.php
+++ views/personal/organization-statistic.php
@@ -5,57 +5,10 @@ use yii\widgets\ActiveForm;
 use yii\helpers\Url;
 
 /* @var $this yii\web\View */
+
 $this->title = 'Информация';
-   $this->params['breadcrumbs'][] = 'Информация';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-
-<?php /* if ($informsProvider->getTotalCount() > 0) { ?>
-    <div class="modal fade">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title">Оповещения</h4>
-          </div>
-          <div class="modal-body">
-            <?= GridView::widget([
-                'dataProvider' => $informsProvider,
-                'summary' => false,
-                'showHeader' => false,
-                'columns' => [
-                    // 'id',
-                    // 'contract_id',
-                    // 'from',
-                    'date',
-                    'text:ntext',
-                    'program_id',
-                    // 'read',
-
-                    ['class' => 'yii\grid\ActionColumn',
-                        'template' => '{permit} {view}',
-                         'buttons' =>
-                             [
-                                 'permit' => function ($url, $model) {
-                                     return Html::a('<span class="glyphicon glyphicon-ok"></span>', Url::to(['/informs/read', 'id' => $model->id]), [
-                                         'title' => Yii::t('yii', 'Отметить как прочитанное'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-                                'view' => function ($url, $model) {
-                                     return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', Url::to(['/programs/view', 'id' => $model->program_id]), [
-                                         'title' => Yii::t('yii', 'Просмотреть программу'),
-                                        'data-toggle' => 'tooltip',
-                                        'data-placement' => 'top'
-                                     ]); },
-                             ]
-                     ],
-                ],
-            ]); ?>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div>
-<?php } */ ?>
 
 <div class="col-md-10 col-md-offset-1 well">
 <p>Количество сертифицированных программ образовательной организации - <?= $count_programs ?></p>
