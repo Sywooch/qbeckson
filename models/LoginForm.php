972fc33b239ab59bb1c8283c4c4ff6fd6a9c6c11
dottoresole <dr.sunny.exet@gmail.com>
2017-03-10 10:28:02 +0200

    Ошибка при неверном логине

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -72,9 +72,10 @@ class LoginForm extends Model
      */
     public function login()
     {
-        if ($this->validate()) {
+        if ($this->validate() && $this->getUser()) {
             return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600*24*30 : 0);
         }
+        
         return false;
     }
 
