46a65630b6575ecf5f57b58f6c9de0d4dbc595ad
dottoresole <dr.sunny.exet@gmail.com>
2017-04-07 12:12:36 +0300

    Убрал лимит на username

--- models/User.php
+++ models/User.php
@@ -39,7 +39,7 @@ class User extends \yii\db\ActiveRecord
         return [
             [['username'], 'unique'],
             [['username'], 'required'],
-            [['username'], 'string', 'length' => [6, 10]],
+            [['username'], 'string', 'length' => [2, 10]],
             [['confirm', 'oldpassword', 'newpassword'], 'string', 'max' => 10, 'min' => 6],
             [['password', 'access_token', 'auth_key'], 'string', 'max' => 64, 'min' => 6],
             [['newlogin', 'newpass'], 'boolean']
