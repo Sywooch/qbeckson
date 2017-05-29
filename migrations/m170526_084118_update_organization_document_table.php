117a9ab870e41ff5887b346a30b0c045e78cecf9
Domolazov Michael <misha_domolazov@mail.ru>
2017-05-29 10:35:39 +0300

    Исправил опечатку в миграции.

--- migrations/m170526_084118_update_organization_document_table.php
+++ migrations/m170526_084118_update_organization_document_table.php
@@ -4,12 +4,18 @@ use yii\db\Migration;
 
 class m170526_084118_update_organization_document_table extends Migration
 {
+    /**
+     * @inheritdoc
+     */
     public function up()
     {
-        $this-$this->addColumn('organization_document', 'base_url', 'varchar(50) DEFAULT NULL');
-        $this-$this->addColumn('organization_document', 'path', 'varchar(50) DEFAULT NULL');
+        $this->addColumn('organization_document', 'base_url', 'varchar(50) DEFAULT NULL');
+        $this->addColumn('organization_document', 'path', 'varchar(50) DEFAULT NULL');
     }
 
+    /**
+     * @inheritdoc
+     */
     public function down()
     {
         $this->dropColumn('organization_document', 'base_url');
