39945abc0d90ae9e9b710a4a3e89d6c515ff8179
dottoresole <dr.sunny.exet@gmail.com>
2017-06-09 10:31:56 +0300

    фикс ошибки с существующими юзерами

--- models/Import.php
+++ models/Import.php
@@ -41,22 +41,29 @@ class Import extends Model
                 if (!$i++ || intval($data[3]) < 1) {
                     continue;
                 }
-                // Пропускаем, если есть такой юзер
-                if ($cnt = User::find()
-                    ->where(['username' => $data[3]])
-                    ->count()) {
-                    continue;
+                $user = null;
+                // Пропускаем, если есть такой юзер с сертификатом
+                if ($userCount = User::find()->where(['username' => $data[3]])->count()) {
+                    if (!$certificateCount = Certificates::find()->where(['number' => $data[3]])->count()) {
+                        $user = User::find()->where(['username' => $data[3]])->one();
+                    } else {
+                        continue;
+                    }
                 }
-                Yii::$app->db->createCommand()->insert('user', [
-                    'username' => $data[3],
-                    'password' => Yii::$app->security->generatePasswordHash($data[22], 7),
-                ])->execute();
-                $userId = Yii::$app->db->lastInsertID;
-                Yii::$app->db->createCommand()->insert('auth_assignment', [
-                    'item_name' => 'certificate',
-                    'user_id' => $userId,
-                    'created_at' => time(),
-                ])->execute();
+                if (empty($user)) {
+                    Yii::$app->db->createCommand()->insert('user', [
+                        'username' => $data[3],
+                        'password' => Yii::$app->security->generatePasswordHash($data[22], 7),
+                    ])->execute();
+                    $userId = Yii::$app->db->lastInsertID;
+                    Yii::$app->db->createCommand()->insert('auth_assignment', [
+                        'item_name' => 'certificate',
+                        'user_id' => $userId,
+                        'created_at' => time(),
+                    ])->execute();
+                } else {
+                    $userId = $user->id;
+                }
                 $certificateArray[] = [$userId, $data[3], $data[4], $data[5], $this->convertEncoding($data[6]), $this->convertEncoding($data[7]), $this->convertEncoding($data[8]), $this->convertEncoding($data[9]), $data[11], $data[12], $data[20], $data[21]];
             }
             fclose($handle);
