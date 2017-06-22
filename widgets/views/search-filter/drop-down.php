b061e4b28b3c4007cb163b988afda1e9294934ff
dottoresole <dr.sunny.exet@gmail.com>
2017-06-22 11:32:10 +0300

    Prompt

--- widgets/views/search-filter/drop-down.php
+++ widgets/views/search-filter/drop-down.php
@@ -4,5 +4,5 @@ use yii\helpers\Html;
 
 ?>
 <div class="col-md-3">
-    <?php echo $form->field($model, $row['attribute'])->dropDownList($row['data'], ['prompt' => 'Выберите..']) ?>
+    <?php echo $form->field($model, $row['attribute'])->dropDownList($row['data'], ['prompt' => 'Не важно']) ?>
 </div>
