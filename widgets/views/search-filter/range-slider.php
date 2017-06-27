1a473efb20e9fd4c9cf43830b1c220f0351d10b6
dottoresole <dr.sunny.exet@gmail.com>
2017-06-27 13:26:22 +0300

    Шаблон инпута

--- widgets/views/search-filter/range-slider.php
+++ widgets/views/search-filter/range-slider.php
@@ -4,12 +4,14 @@ use kartik\slider\Slider;
 
 ?>
 <div class="col-md-12">
-    <?php echo $form->field($model, $row['attribute'])->widget(Slider::classname(), [
+    <?php echo $form->field($model, $row['attribute'], [
+        'template' => "{label} <div class=\"row\"><div class=\"col-sm-4\">{input}{error}{hint}</div></div>"
+    ])->widget(Slider::classname(), [
         'pluginOptions' => [
             'min' => -1,
             'max' => 150000,
             'step' => 10,
-            'range' => true
+            'range' => true,
         ]
     ])->label($model->getAttributeLabel($row['attribute'])); ?>
 </div>
