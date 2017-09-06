78874223ffb5e0a0a3b74199c200ee8dd9e72105
dottoresole <dr.sunny.exet@gmail.com>
2017-09-06 15:50:30 +0300

    Исправления

--- views/personal/organization-statistic.php
+++ views/personal/organization-statistic.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <?php if ($this->beginCache('organization-statistic-' . $organization->id, ['duration' => 3600])): ?>
 <div class="col-md-10 col-md-offset-1 well">
     <p>Количество сертифицированных программ образовательной организации - <?= Programs::getCountPrograms($organization['id'], 2) ?></p>
-    <p>Количество програм  образовательной организации ожидающих сертификации - <?= Programs::getCountPrograms($organization['id'], 0) + Programs::getCountPrograms($organization['id'], 1) ?></p>
+    <p>Количество программ образовательной организации ожидающих сертификации - <?= Programs::getCountPrograms($organization['id'], 0) + Programs::getCountPrograms($organization['id'], 1) ?></p>
     <p>Максимально допустимое количество детей для обучения по системе персонифицированного финансирования - <?= $organization['max_child'] ?></p>
     <?php $certificatesUsedCount = Contracts::getCountUsedCertificates(null, ['organizationId' => $organization->id]); ?>
     <p>Количество детей обучающихся по системе персонифицированного финансирования - <?= $certificatesUsedCount ?></p>
