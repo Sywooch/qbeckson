5e7ae6361e058a294097d32ea7d4c63c154b7c8f
Ruslan <work36912@rambler.ru>
2017-11-21 00:48:15 +0300

    изменено отображение номиналов групп

--- views/cert-group/index.php
+++ views/cert-group/index.php
@@ -182,16 +182,14 @@ $this->registerJs($js);
     <?php $form = ActiveForm::begin(['id' => 'payer-settings-form', 'enableAjaxValidation' => true]); ?>
     <?= $form->field($payer, 'days_to_first_contract_request')->textInput() ?>
     <?= $form->field($payer, 'days_to_contract_request_after_refused')->textInput() ?>
-
     <p>*В случае если в течение указанных сроков для сертификата ПФ не будут созданы новые заявки на обучение, то сертификат подлежит переводу в тип "сертификат учета".</p>
+    <?= Html::submitButton('сохранить', ['class' => 'btn btn-primary']) ?>
 
     <?= $form->field(Yii::$app->user->identity->payer, 'certificate_can_use_future_balance')->checkbox() ?>
 
     <div data-toogle="tooltip" title="<?= !$payer->canChangePermission() ? 'установить запрет на зачисление на обучение в текущем периоде не возможно до ' . Yii::$app->formatter->asDate(date('Y-m-d', strtotime(\Yii::$app->operator->identity->settings->current_program_date_to . '-2 Month'))) : '' ?>">
         <?= $form->field($contractCreatePermissionConfirmForm, 'certificate_can_create_contract')->checkbox(['class' => 'certificate-can-create-contract', 'disabled' => !$payer->canChangePermission()]) ?>
     </div>
-
-    <?= Html::submitButton('сохранить', ['class' => 'btn btn-primary']) ?>
 
     <!-- окно для отмены создания договоров -->
     <?php Modal::begin([
@@ -207,7 +205,7 @@ $this->registerJs($js);
 
         <?= $form->field($contractCreatePermissionConfirmForm, 'password')->passwordInput(); ?>
 
-        <?= Html::Button('сохранить', ['class' => 'change-permission-to-contract-create btn btn-primary']) ?>
+        <?= Html::Button('подтвердить', ['class' => 'change-permission-to-contract-create btn btn-primary']) ?>
     </div>
 
     <?php Modal::end() ?>
@@ -218,7 +216,7 @@ $this->registerJs($js);
         'header' => 'После сохранения будет снят запрет на заключение новых договоров',
     ]) ?>
 
-    <?= Html::Button('сохранить', ['class' => 'change-permission-to-contract-create btn btn-primary']) ?>
+    <?= Html::Button('подтвердить', ['class' => 'change-permission-to-contract-create btn btn-primary']) ?>
 
     <?php Modal::end() ?>
 
