4854c2324a7d9be045baa84cdeeb58f3fae6a0cb
Копытов Евгений <eugene-kei@mail.ru>
2017-12-25 14:25:49 +0800

    PFDO-169 Организация видит только свои запросы на удаление договоров

--- controllers/organization/CleanupController.php
+++ controllers/organization/CleanupController.php
@@ -2,10 +2,14 @@
 
 namespace app\controllers\organization;
 
+use app\helpers\ArrayHelper;
 use app\models\ContractDeleteApplication;
+use app\models\Contracts;
 use app\models\search\ContractDeleteApplicationSearch;
 use app\models\search\ContractsSearch;
+use app\models\UserIdentity;
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 
 class CleanupController extends Controller
 {
@@ -14,14 +18,19 @@ class CleanupController extends Controller
      */
     public function actionContract()
     {
+        /** @var UserIdentity $identity */
+        $identity = \Yii::$app->user->identity;
         $waitingModel = new ContractDeleteApplicationSearch([
             'status' => ContractDeleteApplication::STATUS_WAITING,
+            'organizationId' => ArrayHelper::getValue($identity, ['organization', 'id']),
         ]);
         $confirmedModel = new ContractDeleteApplicationSearch([
             'status' => ContractDeleteApplication::STATUS_CONFIRMED,
+            'organizationId' => ArrayHelper::getValue($identity, ['organization', 'id']),
         ]);
         $refusedModel = new ContractDeleteApplicationSearch([
             'status' => ContractDeleteApplication::STATUS_REFUSED,
+            'organizationId' => ArrayHelper::getValue($identity, ['organization', 'id']),
         ]);
 
         $dataProvider = $waitingModel->search(\Yii::$app->request->queryParams);
@@ -41,29 +50,48 @@ class CleanupController extends Controller
     /**
      * @param $id
      * @return string|\yii\web\Response
+     * @throws NotFoundHttpException
      */
     public function actionCreate($id)
     {
+        /** @var UserIdentity $identity */
+        $identity = \Yii::$app->user->identity;
+        $organizationId = ArrayHelper::getValue($identity, ['organization', 'id']);
+        // Проверяем есть ли такой контракт у этой организации
+        $contract = Contracts::findOne([
+            'id' => $id,
+            'organization_id' => $organizationId
+        ]);
+        if (!$contract) {
+            throw new NotFoundHttpException('Договор не найден');
+        }
         $model = new ContractDeleteApplication([
             'status' => ContractDeleteApplication::STATUS_WAITING,
-            'contract_id' => $id
+            'contract_id' => $id,
+            'organization_id' => $organizationId,
         ]);
         $model->setScenario($model::SCENARIO_CREATE);
 
-        if ($model->load(\Yii::$app->request->post()) && ($model->contract_id = $id) && $model->save()) {
-            \Yii::$app->session->setFlash('success', 'Запрос успешно отправлен.');
-            return $this->redirect(['contract']);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
+        if ($model->load(\Yii::$app->request->post())) {
+            $model->status = ContractDeleteApplication::STATUS_WAITING;
+            $model->contract_id = $id;
+            $model->organization_id = $organizationId;
+            if ($model->save()) {
+                \Yii::$app->session->setFlash('success', 'Запрос успешно отправлен.');
+                return $this->redirect(['contract']);
+            }
         }
+        return $this->render('create', [
+            'model' => $model,
+        ]);
     }
 
     public function actionContractList()
     {
+        /** @var UserIdentity $identity */
+        $identity = \Yii::$app->user->identity;
         $searchModel = new ContractsSearch([
-            'organization_id' => \Yii::$app->user->identity->organization->id,
+            'organization_id' => ArrayHelper::getValue($identity, ['organization', 'id']),
             'modelName' => 'ContractsSearch',
         ]);
         $dataProvider = $searchModel->search(\Yii::$app->request->queryParams);
