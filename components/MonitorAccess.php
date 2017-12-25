4bffa16002dcc49bd015b1adcc385d00b67840fe
dottoresole <dr.sunny.exet@gmail.com>
2017-12-25 13:55:21 +0300

    Восстановление кода для уполн. организаций

--- components/MonitorAccess.php
+++ components/MonitorAccess.php
@@ -2,18 +2,16 @@
 
 namespace app\components;
 
-use Yii;
 use yii\base\Component;
-use yii\base\Event;
-use yii\web\BadRequestHttpException;
 use app\helpers\PermissionHelper;
+use yii\web\ForbiddenHttpException;
 
 class MonitorAccess extends Component
 {
     public function init() {
         parent::init();
         if (!PermissionHelper::checkMonitorUrl()) {
-            throw new BadRequestHttpException('В доступе отказано.');
+            throw new ForbiddenHttpException('В доступе отказано.');
         }
     }
 }
