260dea1bb07467ac3af903f54f09c9159927b62d
gluck <gluck1986@gmail.com>
2018-01-16 19:14:44 +0400

    PFDO-211 fix miss

--- migrations/m180116_113116_price_data_type_fix.php
+++ migrations/m180116_113116_price_data_type_fix.php
@@ -7,12 +7,12 @@ class m180116_113116_price_data_type_fix extends Migration
     public function safeUp()
     {
         $this->alterColumn(
-            '{{%programs}}',
+            '{{%years}}',
             'price',
             $this->money(12, 2)->null()
         );
         $this->alterColumn(
-            '{{%programs}}',
+            '{{%years}}',
             'normative_price',
             $this->money(12, 2)->notNull()
         );
@@ -21,12 +21,12 @@ class m180116_113116_price_data_type_fix extends Migration
     public function safeDown()
     {
         $this->alterColumn(
-            '{{%programs}}',
+            '{{%years}}',
             'price',
             $this->float()->null()
         );
         $this->alterColumn(
-            '{{%programs}}',
+            '{{%years}}',
             'normative_price',
             $this->float()->notNull()
         );
