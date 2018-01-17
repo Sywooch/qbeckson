ba812f86d238228208eb633fbf798844bdfa5195
Ruslan <work36912@rambler.ru>
2018-01-17 10:38:39 +0300

    исправлена автопролонгация в новую группу

---
+++ views/programs/view/_group-list-for-auto-prolongation-to-new-group.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * часть страницы для отображения списка групп для перевода при автопролонгации в новую группу
+ *
+ * @var $this View
+ * @var $moduleNameList array
+ * @var $group Groups
+ */
+
+use app\models\Groups;
+use kartik\widgets\DepDrop;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\View;
+
+?>
+
+<div class="panel panel-default">
+    <label>Модули программы для перевода</label>
+    <?= Html::dropDownList('module-id', null, $moduleNameList, [
+        'class' => 'form-control',
+        'id' => 'module-id',
+        'prompt' => '-- Не выбран --',
+    ]) ?>
+    <br>
+    <?php if ([] != $moduleNameList): ?>
+        <?= DepDrop::widget([
+            'name' => 'group-id',
+            'options' => [
+                'id' => 'group-id',
+                'data' => [
+                    'group-info-url' => '/programs/get-group-info-for-auto-prolongation',
+                ]
+            ],
+            'pluginOptions' => [
+                'depends' => ['module-id'],
+                'placeholder' => '-- Не выбран --',
+                'url' => Url::to(['programs/get-group-list-for-auto-prolongation?groupId=' . $group->id])
+            ]
+        ]) ?>
+    <?php endif; ?>
+    <br>
+    <div class="group-info"></div>
+</div>
